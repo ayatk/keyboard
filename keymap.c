48aa6889a8aa2ee793f19f1db7672a2cade95ec5
ayatk <git@ayatk.com>
2018-04-28 23:57:24 +0900

    Remove Dvorak and Colemk keymaps

--- keymap.c
+++ keymap.c
@@ -32,8 +32,6 @@ extern uint8_t is_master;
 // entirely and just use numbers.
 enum layer_number {
     _EUCALYN = 0,
-    _COLEMAK,
-    _DVORAK,
     _KEYPAD,
     _AUX,
     _KAUX,
@@ -45,8 +43,6 @@ enum layer_number {
 
 enum custom_keycodes {
     EUCALYN = SAFE_RANGE,
-    COLEMAK,
-    DVORAK,
     KEYPAD,
     EISU,
     KANA,
@@ -87,49 +83,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,    KC_A,        KC_S,    KC_D,    KC_F,    KC_G,                                           KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN,    KC_RCTL,
         KC_LSFT,    KC_Z,        KC_X,    KC_C,    KC_V,    KC_B,   KC_GRV,              KC_QUOT,           KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,    KC_RSFT,
         MO(_LOWER), MO(_LOWER), KC_CAPS, KC_LALT, KC_LGUI, KC_SPC, LT(_RAISE, KC_BSPC), LT(_RAISE, KC_ENT), KC_SPC, KC_RGUI, KC_RALT, KC_APP, MO(_LOWER), MO(_LOWER)
-    ),
-
-    /* Colemak
-     * ,-----------------------------------------.             ,-----------------------------------------.
-     * | ESC  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Bksp |
-     * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-     * | Tab  |   Q  |   W  |   F  |   P  |   G  |             |   J  |   L  |   U  |   Y  |   ;  | \    |
-     * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-     * | Ctrl |   A  |   R  |   S  |   T  |   D  |             |   H  |   N  |   E  |   I  |   O  | Ctrl |
-     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-     * | Shift|   Z  |   X  |   C  |   V  |   B  |   `  |   '  |   K  |   M  |   ,  |   .  |   /  | Shift|
-     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-     * |Lower | Lower| Caps  | GUI  | Alt | Space|  BS  | Enter| Space| Alt  | GUI  | Menu |Lower |Lower |
-     * `-------------------------------------------------------------------------------------------------'
-     */
-    [_COLEMAK] = KEYMAP(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                      KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS, \
-        KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                      KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_RCTL, \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV, KC_QUOT,  KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-        MO(_LOWER), MO(_LOWER),  KC_CAPS, KC_LALT, KC_LGUI, KC_SPC,  LT(_RAISE, KC_BSPC), \
-        LT(_RAISE, KC_ENT), KC_SPC,  KC_RGUI, KC_RALT, KC_APP, MO(_LOWER), MO(_LOWER) \
-    ),
-
-    /* Dvorak
-     * ,-----------------------------------------.             ,-----------------------------------------.
-     * | ESC  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Bksp |
-     * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-     * | Tab  |   '  |   ,  |   .  |   P  |   Y  |             |   F  |   G  |   C  |   R  |   L  |  \   |
-     * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-     * | Ctrl |   A  |   O  |   E  |   U  |   I  |             |   D  |   H  |   T  |   N  |   S  | Ctrl |
-     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-     * | Shift|   ;  |   Q  |   J  |   K  |   X  |   `  |   /  |   B  |   M  |   W  |   V  |   Z  | Shift|
-     * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-     * |Lower | Lower| Caps  | GUI  | Alt | Space|  BS  | Enter| Space| Alt  | GUI  | Menu |Lower |Lower |
-     * `-------------------------------------------------------------------------------------------------'
-     */
-    [_DVORAK] = KEYMAP(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSLS,
-        KC_LCTL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_RCTL,
-        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_GRV, KC_SLSH,  KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
-        MO(_LOWER), MO(_LOWER),  KC_CAPS, KC_LALT, KC_LGUI, KC_SPC,  LT(_RAISE, KC_BSPC), LT(_RAISE, KC_ENT), KC_SPC,  KC_RGUI, KC_RALT, KC_APP, MO(_LOWER), MO(_LOWER)
     ),
 
     /* Keypad
@@ -239,7 +192,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Adjust (Lower + Raise)
      * ,-----------------------------------------.             ,-----------------------------------------.
-     * |      |      |Keypad|Dvorak|Colemk|Qwerty|             |Qwerty|Colemk|Dvorak|Keypad|      |      |
+     * |      |      |Keypad|      |      |EUCALYN|             |EUCALYN|      |      |Keypad|      |      |
      * |------+------+------+------+------+------|             |------+------+------+------+------+------|
      * |      | Reset|RGBRST|RGB ON|Aud on| Win  |             | Win  |Aud on|RGB ON|RGBRST|      |      |
      * |------+------+------+------+------+------|             |------+------+------+------+------+------|
@@ -251,7 +204,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-------------------------------------------------------------------------------------------------'
      */
     [_ADJUST] =  KEYMAP(
-        XXXXXXX, XXXXXXX, KEYPAD,  DVORAK,   COLEMAK, EUCALYN,                   EUCALYN, COLEMAK, DVORAK,   KEYPAD, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, KEYPAD,  XXXXXXX, XXXXXXX, EUCALYN,                   EUCALYN, XXXXXXX, XXXXXXX,   KEYPAD, XXXXXXX, XXXXXXX,
         XXXXXXX, RESET,   RGBRST,  RGB_TOG,   AU_ON, AG_SWAP,                   AG_SWAP,   AU_ON, RGB_TOG,  RGBRST, XXXXXXX, XXXXXXX,
         RGB_HUI, RGB_SAI, RGB_VAI, RGB_SMOD,  AU_OFF, AG_NORM,                   AG_NORM,  AU_OFF, RGB_SMOD, RGB_VAI, RGB_SAI, RGB_HUI,
         RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, RGB_VAD, RGB_SAD, RGB_HUD,
@@ -287,8 +240,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #ifdef AUDIO_ENABLE
     float tone_eucalyn[][2]    = SONG(EUCALYN_SOUND);
-    float tone_dvorak[][2]     = SONG(DVORAK_SOUND);
-    float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
     float tone_plover[][2]     = SONG(PLOVER_SOUND);
     float tone_plover_gb[][2]  = SONG(PLOVER_GOODBYE_SOUND);
     float music_scale[][2]     = SONG(MUSIC_SCALE_SOUND);
@@ -298,13 +249,7 @@ static int current_default_layer;
 
 uint32_t default_layer_state_set_kb(uint32_t state) {
     // 1<<_EUCALYN  - 1 == 1 - 1 == _EUCALYN (=0)
-    // 1<<_COLEMAK - 1 == 2 - 1 == _COLEMAK (=1)
     current_default_layer = state - 1;
-
-    // 1<<_DVORAK  - 2 == 4 - 2 == _DVORAK (=2)
-    if (current_default_layer == 3) {
-        current_default_layer -= 1;
-    }
 
     // 1<<_KEYPAD  - 5 == 8 - 5 == _KEYPAD (=3)
     if (current_default_layer == 7) {
@@ -337,28 +282,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             PLAY_SONG(tone_eucalyn);
             #endif
             update_base_layer(_EUCALYN);
-        }
-
-        return false;
-        break;
-
-    case COLEMAK:
-        if (record->event.pressed) {
-            #ifdef AUDIO_ENABLE
-            PLAY_SONG(tone_colemak);
-            #endif
-            update_base_layer(_COLEMAK);
-        }
-
-        return false;
-        break;
-
-    case DVORAK:
-        if (record->event.pressed) {
-            #ifdef AUDIO_ENABLE
-            PLAY_SONG(tone_dvorak);
-            #endif
-            update_base_layer(_DVORAK);
         }
 
         return false;
@@ -499,8 +422,6 @@ static void render_logo(struct CharacterMatrix *matrix) {
 }
 
 static const char Eucalyn_name[] PROGMEM = " Eucalyn";
-static const char Colemak_name[] PROGMEM = " Colemak";
-static const char Dvorak_name[]  PROGMEM = " Dvorak";
 static const char Keypad_name[]  PROGMEM = " Keypad";
 
 static const char AUX_name[]     PROGMEM = ":AUX";
@@ -512,8 +433,6 @@ static const char Adjust_name[]  PROGMEM = ":Adjust";
 
 static const char *layer_names[] = {
     [_EUCALYN] = Eucalyn_name,
-    [_COLEMAK] = Colemak_name,
-    [_DVORAK] = Dvorak_name,
     [_KEYPAD] = Keypad_name,
     [_AUX]    = AUX_name,
     [_KAUX]   = KAUX_name,
