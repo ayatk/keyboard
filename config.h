b8ebc657ded107ac71eeaa72980a072fd3f3c42b
ayatk <git@ayatk.com>
2018-04-28 18:50:02 +0900

    reformat code

--- config.h
+++ config.h
@@ -38,7 +38,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Helix keyboard OLED support
 //      see ./rules.mk: OLED_ENABLE=yes or no
 #ifdef OLED_ENABLE
-  #define SSD1306OLED
+    #define SSD1306OLED
 #endif
 
 /* Select rows configuration */
@@ -47,15 +47,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* key matrix size */
 // Rows are doubled-up
 #if  HELIX_ROWS == 4
-  #define MATRIX_ROWS 8
-  #define MATRIX_COLS 7
-  #define MATRIX_ROW_PINS { D4, C6, D7, E6 }
+    #define MATRIX_ROWS 8
+    #define MATRIX_COLS 7
+    #define MATRIX_ROW_PINS { D4, C6, D7, E6 }
 #elif HELIX_ROWS == 5
-  #define MATRIX_ROWS 10
-  #define MATRIX_COLS 7
-  #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
+    #define MATRIX_ROWS 10
+    #define MATRIX_COLS 7
+    #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
 #else
-  #error "expected HELIX_ROWS 4 or 5"
+    #error "expected HELIX_ROWS 4 or 5"
 #endif
 
 #define USE_SERIAL_PD2
@@ -69,54 +69,54 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define RGBLIGHT_ANIMATIONS : see ./rules.mk: LED_ANIMATIONS = yes or no
 //    see ./rules.mk: LED_BACK_ENABLE or LED_UNDERGLOW_ENABLE set yes
 #ifdef RGBLED_BACK
-  #if HELIX_ROWS == 4
-    #define RGBLED_NUM 25
-  #elif HELIX_ROWS == 5
-    #define RGBLED_NUM 32
-  #endif
+    #if HELIX_ROWS == 4
+        #define RGBLED_NUM 25
+    #elif HELIX_ROWS == 5
+        #define RGBLED_NUM 32
+    #endif
 #else
-  #define RGBLED_NUM 6
+    #define RGBLED_NUM 6
 #endif
 
 #ifndef IOS_DEVICE_ENABLE
-  #if RGBLED_NUM <= 6
-    #define RGBLIGHT_LIMIT_VAL 255
-  #else
-    #if HELIX_ROWS == 5
-      #define RGBLIGHT_LIMIT_VAL 120
+    #if RGBLED_NUM <= 6
+        #define RGBLIGHT_LIMIT_VAL 255
     #else
-      #define RGBLIGHT_LIMIT_VAL 130
+        #if HELIX_ROWS == 5
+            #define RGBLIGHT_LIMIT_VAL 120
+        #else
+            #define RGBLIGHT_LIMIT_VAL 130
+        #endif
     #endif
-  #endif
-  #define RGBLIGHT_VAL_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
 #else
-  #if RGBLED_NUM <= 6
-    #define RGBLIGHT_LIMIT_VAL 90
-  #else
-    #if HELIX_ROWS == 5
-      #define RGBLIGHT_LIMIT_VAL 35
+    #if RGBLED_NUM <= 6
+        #define RGBLIGHT_LIMIT_VAL 90
     #else
-      #define RGBLIGHT_LIMIT_VAL 45
+        #if HELIX_ROWS == 5
+            #define RGBLIGHT_LIMIT_VAL 35
+        #else
+            #define RGBLIGHT_LIMIT_VAL 45
+        #endif
     #endif
-  #endif
-  #define RGBLIGHT_VAL_STEP 4
+    #define RGBLIGHT_VAL_STEP 4
 #endif
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 
 
 #if defined(RGBLIGHT_ENABLE) && !defined(IOS_DEVICE_ENABLE)
-// USB_MAX_POWER_CONSUMPTION value for Helix keyboard
-//  120  RGBoff, OLEDoff
-//  120  OLED
-//  330  RGB 6
-//  300  RGB 32
-//  310  OLED & RGB 32
-  #define USB_MAX_POWER_CONSUMPTION 400
+    // USB_MAX_POWER_CONSUMPTION value for Helix keyboard
+    //  120  RGBoff, OLEDoff
+    //  120  OLED
+    //  330  RGB 6
+    //  300  RGB 32
+    //  310  OLED & RGB 32
+    #define USB_MAX_POWER_CONSUMPTION 400
 #else
-  // fix iPhone and iPad power adapter issue
-  // iOS device need lessthan 100
-  #define USB_MAX_POWER_CONSUMPTION 100
+    // fix iPhone and iPad power adapter issue
+    // iOS device need lessthan 100
+    #define USB_MAX_POWER_CONSUMPTION 100
 #endif
 
 #endif /* CONFIG_USER_H */
